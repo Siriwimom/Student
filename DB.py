a3cc141a46c88e167876449037761b8465d5fb60
Siriwimom <155601816+Siriwimom@users.noreply.github.com>
2024-01-25 12:46:56 +0700

    Add files via upload

---
+++ DB.py
@@ -0,0 +1,54 @@
+from pymongo.mongo_client import MongoClient
+
+def LoginDB():
+    url = "mongodb+srv://123456:pandawar0011sss@cluster0.x1hwgox.mongodb.net/"
+    client = MongoClient(url)
+    client.admin.command('ping')
+    db = client["students"]
+    collection = db["std_info"]
+    return db,collection
+
+def ShowAllStudentRecord():
+    db, collection = LoginDB()
+    all_students = collection.find()
+    return all_students
+
+def AddStudentRecord(id, name, major, gpa):
+    db, collection = LoginDB()
+    try:
+        collection.insert_one({"_id": id,
+                               "fullname": name,
+                               "major": major,
+                               "gpa": gpa
+                               })
+    except Exception as e:
+        print(e)
+
+def UpdateStudentRecord(oldID, newFullName, newMajor, newGPA):
+    db, collection = LoginDB()
+    try:
+        collection.update_one(
+            {"_id": oldID},
+            {"$set": {
+                "fullname": newFullName,
+                "major": newMajor,
+                "gpa": newGPA}}
+        )
+    except Exception as e:
+        print(e)
+
+def DeleteStudentRecord(id):
+    db, collection = LoginDB()
+    try:
+        collection.delete_one(
+            {"_id": id}
+        )
+    except Exception as e:
+        print(e)
+
+def FindStudentRecord(id):
+    db, collection = LoginDB()
+    try:
+        return collection.find_one({"_id": id})
+    except Exception as e:
+        print(e)
---
+++ studentAPI.py
@@ -0,0 +1,77 @@
+from DB import *
+from flask import Flask, jsonify, request
+from flask_basicauth import BasicAuth
+
+
+app = Flask(__name__)
+
+app.config['BASIC_AUTH_USERNAME']='username'
+app.config['BASIC_AUTH_PASSWORD']='password'
+basic_auth = BasicAuth(app)
+
+@app.route("/")
+def Welcome():
+    return jsonify("Welcome to Student Management API")
+
+@app.route("/students")
+@basic_auth.required
+def ShowAllStudents():
+    students = ShowAllStudentRecord()
+    studentsList = []
+    for student in students:
+        studentsList.append(student)
+    return jsonify({"studentsList": studentsList})
+
+
+@app.route("/students/<std_id>")
+@basic_auth.required
+def ShowStudent(std_id):
+    if FindStudentRecord(std_id) == None:
+        return jsonify({"error": "Student not found"}), 404
+    else:
+        return jsonify(FindStudentRecord(std_id))
+
+
+@app.route("/students", methods=["POST"])
+@basic_auth.required
+def AddStudent():
+    data = request.get_json()
+    ID = data.get('_id')
+    name = data.get('fullname')
+    major = data.get('major')
+    GPA = data.get('gpa')
+
+
+    if FindStudentRecord(ID) == None:
+        AddStudentRecord(ID, name, major, GPA)
+        return jsonify(FindStudentRecord(ID))
+    else:
+        return jsonify({"error": "Cannot create new student"}, 500)
+
+@app.route("/students/<std_id>", methods=["DELETE"])
+@basic_auth.required
+def DeleteStudent(std_id):
+    if FindStudentRecord(std_id) == None:
+        return jsonify({"error": "Student not found"}), 404
+    else:
+        DeleteStudentRecord(std_id)
+        return jsonify({"message":"Student deleted successfully"}), 200
+
+
+@app.route("/students/<std_id>", methods=["PUT"])
+@basic_auth.required
+def UpdateStudent(std_id):
+    data = request.get_json()
+    name = data.get('fullname')
+    major = data.get('major')
+    GPA = data.get('gpa')
+
+    if FindStudentRecord(std_id) == None:
+        return jsonify({"error": "Cannot update student"}), 404
+    else:
+        UpdateStudentRecord(std_id, name, major, GPA)
+        return jsonify(FindStudentRecord(std_id))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
